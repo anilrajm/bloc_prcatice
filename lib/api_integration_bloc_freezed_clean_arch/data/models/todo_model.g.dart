2190e65238dbad5b0e93317b79fd4412b366ad4f
Anil Raj <131244294+anilrajm@users.noreply.github.com>
2024-10-03 00:55:10 +0530

    add freezed clean arch and sample api integration

---
+++ lib/api_integration_bloc_freezed_clean_arch/data/models/todo_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$TodoModelImpl _$$TodoModelImplFromJson(Map<String, dynamic> json) =>
+    _$TodoModelImpl(
+      id: (json['id'] as num).toInt(),
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$$TodoModelImplToJson(_$TodoModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+    };
---
+++ lib/api_integration_bloc_freezed_clean_arch/domain/entities/todo.freezed.dart
@@ -0,0 +1,59 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'todo.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$Todo {
+  int get id => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+
+class _$TodoImpl implements _Todo {
+  const _$TodoImpl({required this.id, required this.title});
+
+  @override
+  final int id;
+  @override
+  final String title;
+
+  @override
+  String toString() {
+    return 'Todo(id: $id, title: $title)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TodoImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.title, title) || other.title == title));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, id, title);
+}
+
+abstract class _Todo implements Todo {
+  const factory _Todo({required final int id, required final String title}) =
+      _$TodoImpl;
+
+  @override
+  int get id;
+  @override
+  String get title;
+}
